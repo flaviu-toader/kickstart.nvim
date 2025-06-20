3f6552a91bea91dbc03f2d23aa859c74d08892f8
Flaviu <flaviu.toader@egym.com>
2025-06-20 14:15:31 +0200

    Remove remote-nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,54 +4,6 @@
 -- See the kickstart.nvim README for more information
 return {
   {
-    'amitds1997/remote-nvim.nvim',
-    version = '*', -- Pin to GitHub releases
-    dependencies = {
-      'nvim-lua/plenary.nvim', -- For standard functions
-      'MunifTanjim/nui.nvim', -- To build the plugin UI
-      'nvim-telescope/telescope.nvim', -- For picking b/w different remote methods
-    },
-    config = function()
-      require('remote-nvim').setup {
-        client_callback = function(port, workspace_config)
-          local cmd = ('wezterm cli set-tab-title --pane-id $(wezterm cli spawn nvim --server localhost:%s --remote-ui) %s'):format(
-            port,
-            ("'Remote: %s'"):format(workspace_config.host)
-          )
-          if vim.env.TERM == 'tmux-256color' then
-            cmd = ('tmux new-window -n remote nvim --server localhost:%s --remote-ui'):format(port)
-          end
-          if vim.env.TERM == 'xterm-kitty' then
-            -- cmd = ("kitty -e nvim --server localhost:%s --remote-ui"):format(port)
-            cmd = ("kitten @ launch --type tab --title 'remote' nvim --server localhost:%s --remote-ui"):format(port)
-          end
-          if vim.g.neovide then
-            cmd = ('neovide --server localhost:%s'):format(port)
-          end
-          vim.fn.jobstart(cmd, {
-            detach = true,
-            on_exit = function(job_id, exit_code, event_type)
-              -- This function will be called when the job exits
-              print('Client', job_id, 'exited with code', exit_code, 'Event type:', event_type)
-            end,
-          })
-        end,
-        remote = {
-          copy_dirs = {
-            config = {
-              base = '/home/flaviu/.config/nvim',
-              dirs = '*',
-              compression = {
-                enabled = false, -- Should compression be enabled or not
-                additional_opts = {}, -- Any additional options that should be used for compression. Any argument that is passed to `tar` (for compression) can be passed here as separate elements.
-              },
-            },
-          },
-        },
-      }
-    end,
-  },
-  {
     'stevearc/oil.nvim',
     opts = {},
     dependencies = { 'nvim-tree/nvim-web-devicons' },
