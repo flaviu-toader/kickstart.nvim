3812e1ec3d76b3dcd23c647fb6bc6b2be47d1af4
Flaviu <flaviu.toader@egym.com>
2025-11-12 13:51:03 +0100

    Remove codecompanion.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -34,37 +34,6 @@ return {
     },
   },
   {
-    'olimorris/codecompanion.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    config = function()
-      require('codecompanion').setup {
-        adapters = {
-          ollama = function()
-            return require('codecompanion.adapters').extend('ollama', {
-              name = 'ollama',
-              schema = {
-                model = {
-                  default = 'deepseek-r1:14b',
-                },
-              },
-            })
-          end,
-        },
-        strategies = {
-          chat = {
-            adapter = 'ollama',
-          },
-          inline = {
-            adapter = 'ollama',
-          },
-        },
-      }
-    end,
-  },
-  {
     'github/copilot.vim',
   },
   {
