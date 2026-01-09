07238b3c4cf09e71a30e87c1288c2b7bd41f9f43
Flaviu <flaviu.toader@egym.com>
2026-01-09 09:12:28 +0100

    add lazy flag for java. change term esc sequence

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,7 +5,7 @@
 
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
@@ -56,6 +56,8 @@ return {
   },
   {
     'nvim-java/nvim-java',
+    lazy = true,
+    ft = { 'java' },
     config = function()
       require('java').setup {}
       vim.lsp.enable 'jdtls'
