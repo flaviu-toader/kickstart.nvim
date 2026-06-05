5fcff801e73f48bb24153f8b1934c65e8cd348e4
Flaviu Toader <flaviu.toader@gmail.com>
2026-06-05 09:58:03 +0200

    enable zls

--- init.lua
+++ init.lua
@@ -608,6 +608,7 @@ require('lazy').setup({
         eslint = {},
         rust_analyzer = {},
         gopls = {},
+        zls = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
